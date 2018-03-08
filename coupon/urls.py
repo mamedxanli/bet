7873651a42f9b19d487c11d1ebd95933b765c135
mamedxanli <mamedxanli@gmail.com>
2018-03-08 17:52:30 +0100

    no comments

--- coupon/urls.py
+++ coupon/urls.py
@@ -4,7 +4,6 @@ from coupon import views
 from django.urls import path
 
 
-app_name = 'coupon'
 urlpatterns = [
     url(r'^$', views.EmptyCoupon.as_view(), name='coupon'),
     path('<coupon>/', views.show_coupon, name='show_coupon')
