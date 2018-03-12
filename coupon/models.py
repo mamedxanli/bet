c32756a94c9ed9581e43d7bc75e3e13276c91298
mamedxanli <mamedxanli@gmail.com>
2018-03-12 16:32:03 +0100

    removing bet4 field from copn models

--- coupon/models.py
+++ coupon/models.py
@@ -3,7 +3,7 @@ from games.models import Games
 
 class Coupon(models.Model):
     coupon_tour = models.ForeignKey(Games, on_delete=models.CASCADE)
-    coupon_id = models.CharField("Coupon id", max_length=30)
+    coupon_id = id
     firstname = models.CharField("First Name", max_length=30)
     lastname = models.CharField("Surname", max_length=30)
     phone_number = models.CharField("Phone number", max_length=20)
@@ -13,7 +13,6 @@ class Coupon(models.Model):
     bet1 = models.CharField("Bet 1", max_length=3)
     bet2 = models.CharField("Bet 2", max_length=3)
     bet3 = models.CharField("Bet 3", max_length=3)
-    bet4 = models.CharField("Bet 4", max_length=3)
 
 
 
