054a7e1d90ed1464c15c57d38165268f59412470
Franco <loyolafranco@outlook.com>
2018-03-18 13:50:56 +0100

    works with hard-coded values in form __init__, have to check how to use a queryset!!!!!

--- coupon/forms.py
+++ coupon/forms.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.forms import (ModelForm, ValidationError, CharField)
-from coupon.models import Coupon
+from .models import Coupon, Games
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -19,6 +19,14 @@ class CouponForm(ModelForm):
         'bet1',
         'bet2',
         'bet3',
-
-
         )
+    
+    """
+    def __init__(self, *args, **kwargs):
+    super(EventForm, self).__init__(*args, **kwargs)
+    if not self.instance.actual_start_time:
+        self.initial['actual_start_time'] = self.instance.plan_start_time
+    """
+    def __init__(self, *args, **kwargs):
+        super(CouponForm, self).__init__(*args, **kwargs)
+        self.initial['coupon_tour'] = 0 #self.instance.coupon_tour(Games.objects.filter(games_tour=0))
