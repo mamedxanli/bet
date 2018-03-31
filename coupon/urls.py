8a26eb20594c2b7f55b27c1442ee9bc10a40f940
mamedxanli <mamedxanli@gmail.com>
2018-03-31 20:31:10 +0200

    done

--- coupon/urls.py
+++ coupon/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.CouponCreate.as_view(), name='coupon'),
-    url(r'^coupon_submitted/$', views.CouponSubmitted.as_view(), name='coupon_submitted'),
+    url(r'^coupon_submitted/(?P<pk>\d+)/$', views.CouponSubmitted.as_view(), name='coupon_submitted'),
               ]
