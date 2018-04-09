05eab3e706381e72e8e46c42f977b2519abd7f73
Franco <loyolafranco@outlook.com>
2018-04-09 19:45:28 +0200

    added urls and basic templates

--- bet/urls.py
+++ bet/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     url(r'^coupon/', include('coupon.urls')),
     url(r'^games/', include('games.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
